cca5ed4bd56b1a3aaff0b7b39b5559499f30bfec
AndresTS <atorress@ucenfotec.ac.cr>
2025-03-11 22:17:07 -0600

    Add SCSS styles for Navbar and update Sass dependency to 1.85.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,121 +3,20 @@
 import LightEffect from "@/components/LightEffect/LightEffect";
 import Navbar from "@/components/Navbar/Navbar";
 import { useLanguage } from "@/context/languageContext";
-import { NextIntlClientProvider, useTranslations } from "next-intl";
-import Image from "next/image";
+import { NextIntlClientProvider } from "next-intl";
 import messagesEs from "../../messages//es.json";
 import messagesEn from "../../messages/en.json";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 
 function MainContent() {
-  
-  const t = useTranslations("HomePage");
+  // const t = useTranslations("HomePage");
 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <Navbar />
         <LightEffect />
-        <div className={styles.logoContainer}>
-          <Image
-            className={styles.mainLogo}
-            src="/logo.png"
-            alt="Logo Principal"
-            width={100}
-            height={100}
-            priority
-          />
-        </div>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt={t("nextLogoAlt")}
-          width={180}
-          height={38}
-          priority
-        />
-
-        <h1>{t("welcome")}</h1>
-        <p>{t("description")}</p>
-
-        <ol>
-          <li>
-            {t("getStarted")} <code>src/app/page.tsx</code>
-          </li>
-          <li>{t("saveChanges")}</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt={t("vercelLogoAlt")}
-              width={20}
-              height={20}
-            />
-            {t("deploy")}
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            {t("readDocs")}
-          </a>
-        </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt={t("fileIconAlt")}
-            width={16}
-            height={16}
-          />
-          {t("learn")}
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt={t("windowIconAlt")}
-            width={16}
-            height={16}
-          />
-          {t("examples")}
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt={t("globeIconAlt")}
-            width={16}
-            height={16}
-          />
-          {t("visitNext")}
-        </a>
-      </footer>
     </div>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,44 +1,60 @@
 "use client";
 
 import { useLanguage } from "@/context/languageContext";
-import Link from "next/link";
+import Image from "next/image";
 import ReactCountryFlag from "react-country-flag";
+import styles from "./Navbar.module.scss";
 
 export default function Navbar() {
   const { locale, setLocale } = useLanguage();
 
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">🏠 {locale === "es" ? "Inicio" : "Home"}</Link>
-        </li>
-        <li>
-          <Link href="/about">
-            ℹ️ {locale === "es" ? "Sobre Nosotros" : "About Us"}
-          </Link>
-        </li>
+    <nav className={`py-3 ${styles.navbar}`}>
+      <div className="container d-flex justify-content-between align-items-center">
+        {/* Logo */}
+        <div className={styles["navbar-logo"]}>
+          <Image src="/logo.png" alt="VisionDev Logo" width={60} height={60} />
+          <span className={styles["navbar-brand"]}>visiondev</span>
+        </div>
 
-        <li>
+        {/* Enlaces de Navegación */}
+        <div className={`d-flex gap-3 ${styles["navbar-links"]}`}>
+          {["proyectos", "visión", "contacto"].map((item) => (
+            <a key={item} href="#">
+              {item}
+            </a>
+          ))}
+        </div>
+
+        {/* Selector de idioma */}
+        <div className="d-flex align-items-center">
           🌍 {locale === "es" ? "Idioma" : "Language"}:
-          <button onClick={() => setLocale("en")} disabled={locale === "en"}>
-            <ReactCountryFlag 
-              countryCode="US" 
-              svg 
-              style={{ width: '1em', height: '1em', marginRight: '5px' }} 
+          <button
+            onClick={() => setLocale("en")}
+            disabled={locale === "en"}
+            className="btn btn-sm btn-outline-primary mx-2"
+          >
+            <ReactCountryFlag
+              countryCode="US"
+              svg
+              className={styles["language-btn"]}
             />
             English
           </button>
-          <button onClick={() => setLocale("es")} disabled={locale === "es"}>
-            <ReactCountryFlag 
-              countryCode="ES" 
-              svg 
-              style={{ width: '1em', height: '1em', marginRight: '5px' }} 
+          <button
+            onClick={() => setLocale("es")}
+            disabled={locale === "es"}
+            className="btn btn-sm btn-outline-secondary"
+          >
+            <ReactCountryFlag
+              countryCode="ES"
+              svg
+              className={styles["language-btn"]}
             />
             Español
           </button>
-        </li>
-      </ul>
+        </div>
+      </div>
     </nav>
   );
 }
