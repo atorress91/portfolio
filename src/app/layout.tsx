491e2c03ab640cf01caf4a41e67428dc13c085f4
AndresTS <andres91411@gmail.com>
2025-04-19 14:30:05 -0600

    Add locale handling to RootLayout component
    Updated the RootLayout to include a `params` parameter for handling locales. The `lang` attribute on the `<html>` tag is now dynamically set based on the locale, with a default of "es". Improved structure ensures better internationalization support.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,22 +2,20 @@ import { LanguageProvider } from "@/context/languageContext";
 import clsx from "clsx";
 import "../styles/style.scss";
 import React from "react";
-import {Metadata} from "next";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
     description: "Portafolio personal Andrés Torres Sánchez",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html>
-      <body className={clsx("flex min-h-[100vh] flex-col bg-slate-100")}>
+export default function RootLayout({children, params,}: { children: React.ReactNode; params: { locale: string }; }) {
+    const locale = params.locale || "es";
+
+    return (
+        <html lang={locale}>
+        <body className={clsx("flex min-h-[100vh] flex-col bg-slate-100")}>
         <LanguageProvider>{children}</LanguageProvider>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
