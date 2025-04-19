12d317badefc3bbbd87658caeaa6775684df6e55
AndresTS <andres91411@gmail.com>
2025-04-19 14:37:49 -0600

    Refactor RootLayout to remove locale parameter.
    Replaced the locale parameter with a defaultLocale constant for simplification. This change ensures consistent behavior while reducing unnecessary props in the RootLayout component.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,11 +8,11 @@ export const metadata: Metadata = {
     description: "Portafolio personal Andrés Torres Sánchez",
 };
 
-export default function RootLayout({children, params,}: { children: React.ReactNode; params: { locale: string }; }) {
-    const locale = params.locale || "es";
+export default function RootLayout({children,}: { children: React.ReactNode; }) {
+    const defaultLocale = "es";
 
     return (
-        <html lang={locale}>
+        <html lang={defaultLocale}>
         <body className={clsx("flex min-h-[100vh] flex-col bg-slate-100")}>
         <LanguageProvider>{children}</LanguageProvider>
         </body>
