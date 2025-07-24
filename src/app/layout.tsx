b159ef2ef1fbd4c65c2053aa5b67bc2011068937
AndresTS <andres91411@gmail.com>
2025-07-24 17:09:45 -0600

    Make `RootLayout` props readonly for better type safety and immutability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
     description: "Portafolio personal Andrés Torres Sánchez",
 };
 
-export default function RootLayout({children,}: { children: React.ReactNode; }) {
+export default function RootLayout({children,}: Readonly<{ children: React.ReactNode; }>) {
     const defaultLocale = "es";
 
     return (
